abf03ae50a7f6c62a46230b2b37f14a5713f1365
Amr Tamer <122938074+AmrTamer23@users.noreply.github.com>
2024-12-02 10:16:05 +0200

    Boilerplate cleanup and config fixes (#3)
    * chore(dashboard): rm unwanted boilerplate
    
    * chore(dashboard): rm unwanted vercel config
    
    * chore(platform): rm unwanted boilerplate
    
    * chore(platform): rm unwanted vercel config
    
    * chore(all): rm unwanted dependecy
    
    * chore(all): rm unwanted dependecy
    
    * chore(all): rm unwanted dependecy
    
    * fix(all): get eslint to work 🎉
    
    * feat(all): corrected internal eslint config
    
    * chore(all): internal eslint config cleanup
    
    * feat(storybook): extend the internal eslint config
    
    * chore(all): lint on pre commit
    
    * chore(all): add tailwind prettier plugin
    
    * fix(storybook): convert eslint to .mjs to drop type module

--- apps/platform/app/page.tsx
+++ apps/platform/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home(): React.ReactNode {
-  return <main>Dashboard</main>
+  return <main>Platform</main>
 }
---
+++ packages/dallah-utils/eslint.config.js
@@ -0,0 +1,2 @@
+import eslintConfigDallah from '../../packages/eslint-config-dallah/index.js'
+export default [...eslintConfigDallah]
