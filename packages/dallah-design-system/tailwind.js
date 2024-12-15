cd82ed2507a7faff54cd156480ed6a82e210b1d3
AmrTamer23 <amrtamer23mj@gmail.com>
2024-12-15 03:20:30 +0200

    feat(all): create a color palette tailwindcss preset

--- packages/dallah-design-system/tailwind.js
+++ packages/dallah-design-system/tailwind.js
@@ -1,7 +1,23 @@
 export default {
   theme: {
     colors: {
-      primary: '#0070f3',
+      background: '#F8F8F8',
+      primary: {
+        DEFAULT: '#234D64',
+        foreground: '#FFFFFF',
+      },
+      secondary: {
+        DEFAULT: '#F7CA71',
+        foreground: '#234D64',
+      },
+      success: {
+        DEFAULT: '#2FC57A',
+        foreground: '#FFFFFF',
+      },
+      destructive: {
+        DEFAULT: '#B44242',
+        foreground: '#FFFFFF',
+      },
     },
   },
 }
