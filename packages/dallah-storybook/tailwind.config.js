911bbd97ac26fc72045c0606d512ac9fb092590e
AmrTamer23 <amrtamer23mj@gmail.com>
2024-12-15 04:00:44 +0200

    feat(storybook): changes on tailwindcss conf

--- packages/dallah-storybook/tailwind.config.js
+++ packages/dallah-storybook/tailwind.config.js
@@ -1,9 +1,11 @@
-import designSystem from '../dallah-design-system/tailwind'
+import designSystem from '../dallah-design-system/tailwind.js'
 
 export default {
-  presets: [designSystem],
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
     './node_modules/@dallah/design-system/src/**/*.{js,jsx,ts,tsx}',
   ],
+  theme: {
+    ...designSystem,
+  },
 }
