659e70b2cd8dd64ced0fafc6b3702f7cb97ab449
AmrTamer23 <amrtamer23mj@gmail.com>
2024-12-17 22:32:57 +0200

    fix(platform): design tokens import

--- apps/platform/tailwind.config.js
+++ apps/platform/tailwind.config.js
@@ -1,10 +1,10 @@
-import designSystem from '../../packages/dallah-design-system/tailwind.js'
+import { designTokens } from '@dallah/design-system'
 export default {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     // Add the external packages that are using Tailwind CSS
   ],
   theme: {
-    ...designSystem,
+    ...designTokens,
   },
 }
