eb4050118e2fbc60a51822915f9908e9c52e4c40
AmrTamer23 <amrtamer23mj@gmail.com>
2024-12-17 04:14:50 +0200

    feat(all): typography design tokens

---
+++ packages/dallah-design-system/src/tokens.js
@@ -0,0 +1,91 @@
+export default {
+  colors: {
+    background: '#F8F8F8',
+    primary: {
+      DEFAULT: '#234D64',
+      foreground: '#FFFFFF',
+    },
+    secondary: {
+      DEFAULT: '#F7CA71',
+      foreground: '#234D64',
+    },
+    success: {
+      DEFAULT: '#2FC57A',
+      foreground: '#FFFFFF',
+    },
+    destructive: {
+      DEFAULT: '#B44242',
+      foreground: '#FFFFFF',
+    },
+  },
+  fontSize: {
+    'display-lg': '180px',
+    'display-md': '128px',
+    'display-sm': '96px',
+    'heading-2xl': '72px',
+    'heading-xl': '60px',
+    'heading-lg': '48px',
+    'heading-md': '36px',
+    'heading-sm': '30px',
+    'heading-xs': '24px',
+    'text-2xl': '24px',
+    'text-xl': '20px',
+    'text-lg': '18px',
+    'text-md': '16px',
+    'text-sm': '14px',
+    'text-xs': '12px',
+    'text-2xs': '10px',
+    'paragraph-2xl': '24px',
+    'paragraph-xl': '20px',
+    'paragraph-lg': '18px',
+    'paragraph-md': '16px',
+    'paragraph-sm': '14px',
+    'paragraph-xs': '12px',
+    'label-2xl': [
+      '20px',
+      {
+        fontWeight: '800',
+      },
+    ],
+    'label-xl': [
+      '18px',
+      {
+        fontWeight: '800',
+      },
+    ],
+    'label-lg': [
+      '16px',
+      {
+        fontWeight: '800',
+      },
+    ],
+    'label-md': [
+      '14px',
+      {
+        fontWeight: '800',
+      },
+    ],
+    'label-sm': [
+      '12px',
+      {
+        fontWeight: '800',
+      },
+    ],
+    'label-xs': [
+      '10px',
+      {
+        fontWeight: '800',
+      },
+    ],
+  },
+  fontWeight: {
+    normal: '400',
+    medium: '500',
+    semibold: '600',
+    bold: '700',
+    'extra-bold': '800',
+  },
+  fontFamily: {
+    sans: ['Sora Variable', 'sans-serif'],
+  },
+}
