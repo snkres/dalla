542227923cc6fa257743b01af9b74acf8b2be757
AmrTamer23 <amrtamer23mj@gmail.com>
2024-12-17 06:21:40 +0200

    feat(storybook): complete normal btn stories

--- packages/dallah-design-system/src/ui/button/button.stories.tsx
+++ packages/dallah-design-system/src/ui/button/button.stories.tsx
@@ -4,27 +4,54 @@ import { Button } from './button'
 const meta: Meta<typeof Button> = {
   title: 'Button',
   component: Button,
+  argTypes: {
+    children: {
+      control: 'text',
+    },
+    disabled: {
+      control: 'boolean',
+    },
+  },
 } as Meta<typeof Button>
 
 export default meta
 
-export const Primary = {
+export const Primary: StoryObj<typeof Button> = {
   args: {
-    children: 'This is a button!',
-    asChild: false,
+    children: 'Learn More',
+    variant: 'default',
+    size: 'default',
   },
 }
 
-export const Secondary = {
+export const Secondary: StoryObj<typeof Button> = {
   args: {
-    children: 'This is a button!',
+    children: 'Learn More',
     variant: 'secondary',
+    size: 'default',
   },
 }
 
-export const Destructive = {
+export const Outline: StoryObj<typeof Button> = {
   args: {
-    children: 'This is a button!',
+    children: 'Learn More',
+    variant: 'outline',
+    size: 'default',
+  },
+}
+
+export const Destructive: StoryObj<typeof Button> = {
+  args: {
+    children: 'Learn More',
     variant: 'destructive',
+    size: 'default',
+  },
+}
+
+export const Ghost: StoryObj<typeof Button> = {
+  args: {
+    children: 'Learn More',
+    variant: 'ghost',
+    size: 'default',
   },
 }
