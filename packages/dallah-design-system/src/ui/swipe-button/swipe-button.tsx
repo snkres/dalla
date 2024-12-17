cefbc838e4bee3366aa1d0bd4d7db2bd449138c3
AmrTamer23 <amrtamer23mj@gmail.com>
2024-12-17 19:26:54 +0200

    feat(all): add utils package in design sys package

--- packages/dallah-design-system/src/ui/swipe-button/swipe-button.tsx
+++ packages/dallah-design-system/src/ui/swipe-button/swipe-button.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react'
 import { motion, PanInfo } from 'framer-motion'
-import { cn } from '../../../lib/utils'
+import { cn } from '@dallah/utils'
 
 interface SwipeButtonProps {
   onSwipeComplete?: () => void
