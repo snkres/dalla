a853d276b66326d72fc25d91d086cc69dfcf13cf
AmrTamer23 <amrtamer23mj@gmail.com>
2024-12-17 08:34:00 +0200

    feat(storybook): added fab story for btn

--- packages/dallah-design-system/src/ui/button/button.stories.tsx
+++ packages/dallah-design-system/src/ui/button/button.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { Button } from './button'
+import React from 'react'
 
 const meta: Meta<typeof Button> = {
   title: 'Button',
@@ -54,4 +55,30 @@ export const Ghost: StoryObj<typeof Button> = {
     variant: 'ghost',
     size: 'default',
   },
+}
+
+export const FAB: StoryObj<typeof Button> = {
+  args: {
+    children: 'Learn More',
+    variant: 'default',
+    size: 'icon',
+  },
+  render: (args) => (
+    <Button {...args}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        className="h-6 w-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+        />
+      </svg>
+    </Button>
+  ),
 }
