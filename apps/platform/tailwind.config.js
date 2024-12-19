96a0678a103caaa172c9b71bf624f899b1f65adb
AmrTamer23 <amrtamer23mj@gmail.com>
2024-12-19 03:45:04 +0200

    feat(platform): wip features section

--- apps/platform/tailwind.config.js
+++ apps/platform/tailwind.config.js
@@ -2,10 +2,13 @@ import { designTokens } from '@dallah/design-system'
 export default {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
     './node_modules/@dallah/design-system/src/**/*.{js,ts,jsx,tsx}',
     // Add the external packages that are using Tailwind CSS
   ],
   theme: {
-    ...designTokens,
+    extend: {
+      ...designTokens,
+    },
   },
 }
