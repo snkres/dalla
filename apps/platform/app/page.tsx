653a88026006090e7e7699769327a26e4e8a7a43
AmrTamer23 <amrtamer23mj@gmail.com>
2024-12-20 17:50:32 +0200

    feat(platform): enahnce dalla ai section w animations

--- apps/platform/app/page.tsx
+++ apps/platform/app/page.tsx
@@ -4,8 +4,12 @@ import { Features } from '@components/landing/features'
 export default function Home(): React.ReactNode {
   return (
     <main className="bg-background min-h-screen py-10">
+      <div className="h-[50dvh]"></div>
       <Features />
+      <div className="h-[50dvh]"></div>
+
       <DallaAi />
+      <div className="h-[50dvh]"></div>
     </main>
   )
 }
