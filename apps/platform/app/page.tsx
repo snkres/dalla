87bf719dfca43e16ab0931bdbe0677d9e3ddfd48
AmrTamer23 <amrtamer23mj@gmail.com>
2024-12-20 18:55:43 +0200

    chore(platform): init boxes section

--- apps/platform/app/page.tsx
+++ apps/platform/app/page.tsx
@@ -1,3 +1,4 @@
+import { Boxes } from '@components/landing/boxes'
 import { DallaAi } from '@components/landing/dalla-ai'
 import { Features } from '@components/landing/features'
 
@@ -10,6 +11,7 @@ export default function Home(): React.ReactNode {
 
       <DallaAi />
       <div className="h-[50dvh]"></div>
+      <Boxes />
     </main>
   )
 }
