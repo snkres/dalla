1aa7314cac1dac1ad78b5d1751832f9e37b3a57f
AmrTamer23 <amrtamer23mj@gmail.com>
2024-12-22 04:11:06 +0200

    feat(platform): minor changes

--- apps/platform/app/page.tsx
+++ apps/platform/app/page.tsx
@@ -7,17 +7,15 @@ import { Navbar } from '@components/landing/layout/navbar'
 
 export default function Home(): React.ReactNode {
   return (
-    <main className="bg-background min-h-screen pb-10">
-      <Hero />
+    <>
       <Navbar />
-      <CTA />
-      <div className="h-[50dvh]"></div>
-      <Features />
-      <div className="h-[50dvh]"></div>
-
-      <DallaAi />
-      <div className="h-[50dvh]"></div>
-      <Boxes />
-    </main>
+      <main className="bg-background min-h-screen py-10">
+        <Hero />
+        <Features />
+        <CTA />
+        <DallaAi />
+        <Boxes />
+      </main>
+    </>
   )
 }
