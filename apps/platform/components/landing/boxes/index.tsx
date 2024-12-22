dbd1073d0315460163603a77e3be67d3e013d301
AmrTamer23 <amrtamer23mj@gmail.com>
2024-12-22 21:15:21 +0200

    feat(platform): adjust spacing between sections

--- apps/platform/components/landing/boxes/index.tsx
+++ apps/platform/components/landing/boxes/index.tsx
@@ -15,10 +15,9 @@ export function Boxes() {
   })
 
   return (
-    <div ref={containerRef} className="mx-auto max-w-[80%] pt-[77px]">
-      <div className="grid gap-y-[32px] md:grid-cols-2">
+    <div ref={containerRef} className="mx-auto max-w-[80%] py-[77px]">
+      <div className="grid gap-x-[32px] gap-y-[32px] md:grid-cols-2">
         <TrustedBox />
-
         <CertifiedBox />
         <FAQ />
         <Rates />
