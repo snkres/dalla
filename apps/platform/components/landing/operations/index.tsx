96327f37897c608cc29fd9bde43910fa2f30ee27
AmrTamer23 <amrtamer23mj@gmail.com>
2024-12-23 15:09:10 +0200

    feat(platform): minor changes

--- apps/platform/components/landing/operations/index.tsx
+++ apps/platform/components/landing/operations/index.tsx
@@ -13,16 +13,16 @@ import { Underline } from '../shared/underline'
 export function Operations() {
   return (
     <section className="relative pt-16">
-      <div className="container mx-auto space-y-14 px-8">
+      <div className="mx-auto space-y-14 px-8 lg:container">
         <div className="text-center">
-          <h2 className="text-slate-blue xl:text-heading-2xl lg:text-heading-xl mb-7 font-bold">
+          <h2 className="text-slate-blue xl:text-heading-2xl lg:text-heading-xl text text-heading-xl mb-7 font-bold">
             Streamline{' '}
             <span className="relative inline-block">
               Operations{' '}
               <Underline className="absolute left-0 z-0 w-full xl:-bottom-6" />
             </span>
           </h2>
-          <p className="xl:text-paragraph-2xl text-paragraph-sm lg:text-paragraph-xl text-gray-500">
+          <p className="xl:text-paragraph-2xl text-paragraph-md lg:text-paragraph-xl text-gray-500">
             Tailored Solutions for Startups, SMEs, and Enterprises
           </p>
         </div>
