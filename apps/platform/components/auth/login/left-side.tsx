bd101f5a22738d1546f6a2dc5ebc24a4c2e6e69e
AmrTamer23 <amrtamer23mj@gmail.com>
2024-12-24 22:55:43 +0200

    feat(platform): extract left side and switch to pro

---
+++ apps/platform/components/auth/login/left-side.tsx
@@ -0,0 +1,51 @@
+import { ModeToggle } from '@components/landing/features/mode-toggle'
+import { LogoHorizontal } from '@dallah/design-system'
+import { cn } from '@dallah/utils'
+import { LoginForm } from './form'
+
+export function LoginLeftSide({
+  mode,
+  setMode,
+}: {
+  mode: 'companies' | 'professional'
+  setMode: (mode: 'companies' | 'professional') => void
+}) {
+  return (
+    <section
+      className={cn(
+        'flex flex-1 flex-col items-center justify-center gap-6',
+        mode === 'professional' ? 'bg-slate-blue text-sunshine-yellow' : '',
+      )}
+    >
+      <LogoHorizontal
+        className={cn(
+          'w-56',
+          mode === 'professional'
+            ? 'fill-foreground'
+            : '[&_path]:fill-slate-blue',
+        )}
+      />
+      <div className="space-y-2">
+        <h1
+          className={cn(
+            'text-heading-md text-center font-semibold',
+            mode === 'professional'
+              ? 'text-sunshine-yellow'
+              : 'text-slate-blue',
+          )}
+        >
+          Log In to Your Dalla Account
+        </h1>
+        <p className="text-paragraph-md mx-auto max-w-md text-center text-[#9A9A9A]">
+          Access your dashboard to connect with experts or manage your
+          consulting projects.
+        </p>
+      </div>
+      <ModeToggle
+        mode={mode as 'companies' | 'professional'}
+        onModeChange={(mode) => setMode(mode as 'companies' | 'professional')}
+      />
+      <LoginForm mode={mode} />
+    </section>
+  )
+}
--- apps/platform/components/auth/login/right-side.tsx
+++ apps/platform/components/auth/login/right-side.tsx
@@ -2,21 +2,32 @@ import { cn } from '@dallah/utils'
 
 export function LoginRightSide({ proMode }: { proMode?: boolean }) {
   return (
-    <section className="relative hidden h-full w-1/2 flex-1 text-white lg:flex">
+    <section
+      className={cn(
+        'relative flex flex-1 items-center justify-center',
+        proMode ? 'bg-slate-blue' : '',
+      )}
+    >
       <img
         src={proMode ? '/pros.webp' : '/company.webp'}
         alt="company"
         className="h-screen w-full rounded-l-[3.5rem] object-cover brightness-75"
       />
       <div className="absolute inset-0 top-0 flex flex-col items-end justify-between">
-        <h1
-          className={cn(
-            'text-heading-2xl line-clamp-3 px-20 py-12 font-semibold transition-all duration-300 ease-in-out',
-            proMode ? 'text-slate-blue' : 'text-foreground',
-          )}
-        >
-          Manage your consulting in style!
-        </h1>
+        {proMode ? (
+          <h1
+            className={cn(
+              'text-heading-2xl text-slate-blue motion-preset-fade line-clamp-3 px-20 py-12 font-semibold transition-all duration-500',
+            )}
+          >
+            Simplify Your Consulting Journey!
+          </h1>
+        ) : (
+          <h1 className="text-heading-2xl motion-preset-fade text-foreground line-clamp-3 px-20 py-12 font-semibold transition-all duration-500">
+            Manage your consulting in style!
+          </h1>
+        )}
+
         <div className="relative bottom-0 right-0 w-auto max-w-[60%]">
           <img
             src="/Dashboard.webp"
