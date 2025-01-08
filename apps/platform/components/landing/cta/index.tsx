8df1b4f986a163f28892d32c9b49e9952e447112
AmrTamer23 <amrtamer23mj@gmail.com>
2025-01-08 22:02:56 +0200

    feat(platform): wip font sizes and spaces

--- apps/platform/components/landing/cta/index.tsx
+++ apps/platform/components/landing/cta/index.tsx
@@ -2,19 +2,19 @@ import { Button } from '@dallah/design-system'
 
 export function CTA() {
   return (
-    <div className="bg-sunshine-yellow my-20 flex w-full flex-col items-center justify-around gap-4 px-[7.5rem] py-14 md:flex-row">
-      <h3 className="text-slate-blue text-heading-lg text-center font-semibold md:text-left">
-        A High-Quality Solution Tailored for Businesses Across MENA
+    <div className="bg-sunshine-yellow my-[77px] flex w-full flex-col items-center justify-around gap-4 px-[7.5rem] py-14 md:flex-row">
+      <h3 className="text-slate-blue text-center text-[2.5rem] font-semibold md:text-left">
+        A High-Quality Solution Tailored <br /> for Businesses Across MENA
       </h3>
       <div className="flex flex-col items-center justify-between gap-2 md:flex-row">
         <Button
-          className="border-slate-blue bg-slate-blue text-sunshine-yellow text-text-md !rounded-full !px-10 !py-6"
+          className="border-slate-blue bg-slate-blue text-sunshine-yellow text-text-lg h-[56px] !rounded-full !px-10 !py-6 font-bold"
           size="lg"
         >
           Get Started
         </Button>
         <Button
-          className="border-slate-blue text-slate-blue text-text-md hover:text-sunshine-yellow hover:bg-slate-blue !rounded-full bg-transparent !px-10 !py-6"
+          className="border-slate-blue text-slate-blue text-text-lg hover:text-sunshine-yellow hover:bg-slate-blue h-[56px] !rounded-full border-2 bg-transparent !px-10 !py-6 font-bold"
           variant="outline"
           size="lg"
         >
