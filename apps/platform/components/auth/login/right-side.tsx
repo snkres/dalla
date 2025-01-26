662c34d89387bff3fe71a81f091ea0a55d495f72
AmrTamer23 <amrtamer23mj@gmail.com>
2025-01-26 19:49:53 +0200

    feat(platform): changes on login page

--- apps/platform/components/auth/login/right-side.tsx
+++ apps/platform/components/auth/login/right-side.tsx
@@ -1,8 +1,4 @@
-import { LogoHorizontal } from '@dallah/design-system'
 import { cn } from '@dallah/utils'
-import { Star, ArrowLeft, ArrowRight } from 'lucide-react'
-import { useState } from 'react'
-import { Testimonials } from './testmonials'
 
 export function LoginRightSide({ proMode }: { proMode?: boolean }) {
   return (
@@ -13,20 +9,11 @@ export function LoginRightSide({ proMode }: { proMode?: boolean }) {
       )}
     >
       <img
-        src="/right-side.webp"
+        src="/login.webp"
         alt="company"
         className="h-screen w-full object-cover object-top brightness-75"
       />
-      <div className="absolute inset-0 top-0 z-20 flex h-screen flex-col items-start justify-between">
-        <LogoHorizontal
-          className={cn(
-            'motion-ease-spring-bouncy z-50 w-72 px-10 py-10 transition-colors duration-500',
-            !proMode ? 'fill-sunshine-yellow-100' : '[&_path]:fill-slate-blue',
-          )}
-        />
 
-        <Testimonials />
-      </div>
     </section>
   )
 }
