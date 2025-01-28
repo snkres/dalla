9cbf8279fbe31aa41641f559f199b35d373001d6
AmrTamer23 <amrtamer23mj@gmail.com>
2025-01-28 17:46:01 +0200

    feat(platform): various changes on styles

--- apps/platform/components/auth/login/form.tsx
+++ apps/platform/components/auth/login/form.tsx
@@ -113,10 +113,10 @@ export function LoginForm({ mode }: { mode: 'companies' | 'professional' }) {
         </div>
       </div>
       <Button
-        className="text-sunshine-yellow-10 shadow-[rgba(16, 24, 40, 0.18)] mt-6 flex w-full items-center justify-center gap-[0.375rem] self-stretch rounded-[0.5rem] border-[0.125rem] border-solid border-[#CEB67B] bg-[#F4D283] stroke-[2px] px-[1rem] py-[10px] shadow-sm"
+        className="text-sunshine-yellow-10 shadow-[rgba(16, 24, 40, 0.18)] mt-6 flex w-full items-center justify-center gap-[0.375rem] self-stretch rounded-[0.5rem] border-[0.05rem] border-solid border-[#CEB67B] bg-[#F4D283] stroke-[0.1px] px-[1rem] py-[10px] shadow-sm"
         type="submit"
         style={{
-          boxShadow: '0px -2px 1px 1px rgba(16, 24, 40, 0.05) inset',
+          boxShadow: '0px -1px 0px 0px rgba(16, 24, 40, 0.1) inset',
         }}
         variant={mode === 'professional' ? 'secondary' : 'default'}
       >
