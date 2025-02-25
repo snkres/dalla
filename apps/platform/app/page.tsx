e84e234aa76c0ead278fa960a45fb648d99cc8b7
AmrTamer23 <amrtamer23mj@gmail.com>
2025-02-25 02:18:40 +0200

    feat(platform): rm landing page

--- apps/platform/app/page.tsx
+++ apps/platform/app/page.tsx
@@ -1,32 +1,12 @@
 'use client'
-import { Operations } from '@components/landing/operations'
-import { CTA } from '@components/landing/cta'
-import { Boxes } from '@components/landing/boxes'
-import { DallaAi } from '@components/landing/dalla-ai'
-import { Features } from '@components/landing/features'
-import { Hero } from '@components/landing/hero'
-import { Navbar } from '@components/landing/layout/navbar'
-import { UnlockingSuccess } from '@components/landing/unlocking-success'
-import { InsightsToEmpower } from '@components/landing/empower'
-import { Footer } from '@components/landing/layout/footer'
-import { useRouter } from 'next/navigation'
 
 export default function Home(): React.ReactNode {
-  window.location.href = '/login'
+
+  window.location.href = '/profile'
+
   return (
-    <>
-      <Navbar />
-      <main className="bg-background min-h-screen w-full py-10">
-        <Hero />
-        <Features />
-        <Operations />
-        <CTA />
-        <DallaAi />
-        <Boxes />
-        <UnlockingSuccess />
-        <InsightsToEmpower />
-      </main>
-      <Footer />
-    </>
+    <main>
+
+    </main>
   )
 }
