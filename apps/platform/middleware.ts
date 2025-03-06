d313cd2943d7cf83db38c68227ec4b9bd8072d06
AmrTamer23 <amrtamer23mj@gmail.com>
2025-03-06 03:38:15 +0200

    refactor(platform): remove debug logging from authentication middleware

--- apps/platform/middleware.ts
+++ apps/platform/middleware.ts
@@ -4,8 +4,6 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('access_token')
 
-  console.log(request.nextUrl.pathname, token)
-
   if (
     !token &&
     !request.nextUrl.pathname.startsWith('/login') &&
