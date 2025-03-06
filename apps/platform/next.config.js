785d8cbe9a30677e9ac01605c0b83dfc21cdc8ff
AmrTamer23 <amrtamer23mj@gmail.com>
2025-03-06 03:03:04 +0200

    chore(platform): remove asset prefix configuration for next.js

--- apps/platform/next.config.js
+++ apps/platform/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: '/docs-static',
   images: {
     remotePatterns: [
       {
@@ -8,18 +7,6 @@ const nextConfig = {
         hostname: 'dalla-dev.fra1.cdn.digitaloceanspaces.com',
       },
     ],
-  },
-  async rewrites() {
-    return {
-      beforeFiles: [
-        // This rewrite is necessary to support assetPrefix only in Next 14 and below.
-        // It is not necessary in Next 15.
-        {
-          source: '/docs-static/_next/:path*',
-          destination: '/_next/:path*',
-        },
-      ],
-    }
   },
 }
 
