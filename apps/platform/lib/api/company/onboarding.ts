788b6e99ffda450e91f54827b9ff5e456da4ef11
AmrTamer23 <amrtamer23mj@gmail.com>
2025-03-20 02:25:09 +0200

    feat(platform): enhance onboarding api responses and update query configurations

--- apps/platform/lib/api/company/onboarding.ts
+++ apps/platform/lib/api/company/onboarding.ts
@@ -18,14 +18,60 @@ export interface CompanyPayload {
     socialLinks: { [key: string]: string }
   }
 }
+export type CompanyOnboardingRes = {
+  statusCode: number
+  success: boolean
+  message: string
+  data: {
+    id: string
+    name: string
+    email: string
+    domain: any
+    password: string
+    onboarded: boolean
+    suspended: boolean
+    verified: boolean
+    createdAt: string
+    updatedAt: string
+    CompanyProfile: {
+      id: string
+      companyId: string
+      location: string
+      areas: Array<{
+        name: string
+        description: string
+      }>
+      goals: Array<{
+        name: string
+        description: string
+      }>
+      targetIndustries: Array<{
+        name: string
+        description: string
+      }>
+      website: string
+      meta: {
+        size: string
+        type: string
+        phone: string
+        industry: string
+        socialLinks: {}
+      }
+      headline: string
+      bio: string
+      logo: string
+      createdAt: string
+      updatedAt: string
+    }
+  }
+  error: any
+  path: string
+  timestamp: string
+}
 
 export async function companyOnboarding(payload: CompanyPayload) {
   const res = await axiosInstance
-    .post<{
-      success: boolean
-      message: string
-      data: CompanyProfile
-    }>('/company/onboarding', payload)
+    .post<CompanyOnboardingRes>('/company/onboarding', payload)
     .then((res) => res.data)
 
   return res
