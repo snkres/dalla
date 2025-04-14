afaafa5e619bef915427140906692ed310c4d088
AmrTamer23 <amrtamer23mj@gmail.com>
2025-04-14 19:47:07 +0200

    fix(platform): update dalla loading to use suspense and improve lottie integration

--- apps/platform/components/shared/dalla-loading.tsx
+++ apps/platform/components/shared/dalla-loading.tsx
@@ -1,14 +1,13 @@
 'use client'
-import React from 'react'
+import React, { Suspense } from 'react'
 import { cn, fadeInVariants } from '@dalla/utils'
 import { motion } from 'motion/react'
+import dynamic from 'next/dynamic'
 
-// import dynamic from 'next/dynamic'
-
-// const DallaLogoLottie = dynamic(
-//   () => import('./dalla-lottie').then((mod) => mod.DallaLogoLottie),
-//   { ssr: false },
-// )
+const DallaLogoLottie = dynamic(
+  () => import('./dalla-lottie').then((mod) => mod.DallaLogoLottie),
+  { ssr: false },
+)
 
 export function DallaLoading({
   title = 'Loading',
@@ -32,9 +31,11 @@ export function DallaLoading({
         animate="animate"
         className="flex flex-col items-center justify-center gap-8"
       >
-        {/* <div className="relative">
-          <DallaLogoLottie width={500} height={500} />
-        </div> */}
+        <div className="relative">
+          <Suspense>
+            <DallaLogoLottie width={500} height={500} />
+          </Suspense>
+        </div>
 
         <div className="-mt-28 flex flex-col items-center gap-2">
           <h1 className="text-2xl font-semibold text-[#234d64]">{title}</h1>
--- apps/platform/components/shared/dalla-lottie.tsx
+++ apps/platform/components/shared/dalla-lottie.tsx
@@ -1,19 +1,18 @@
 'use client'
 import type { FC } from 'react'
-import Lottie from 'lottie-react'
-
+import dallaLottie from './dalla-lottie.json'
+import Lottie from 'react-lottie-player'
 export const DallaLogoLottie: FC<{
   width: number
   height: number
-}> = ({ width, height }) => {
+}> = ({ width = 500, height = 500 }) => {
   return (
     <div>
       <Lottie
-        animationData="/dalla-lottie.json"
+        animationData={dallaLottie}
         loop={true}
-        autoplay={true}
         style={{ width, height }}
-        size={500}
+        play
       />
     </div>
   )
