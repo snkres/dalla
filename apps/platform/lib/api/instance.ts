d84c99467dcb8c3a9a8e3103fd7083d8c3a1708e
AmrTamer23 <amrtamer23mj@gmail.com>
2025-07-13 05:30:22 +0300

    fix(platform): update axios base url to use env

--- apps/platform/lib/api/instance.ts
+++ apps/platform/lib/api/instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://devapi.dalla.app',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true,
 })
 
