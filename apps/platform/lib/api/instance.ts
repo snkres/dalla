67bb01e42470c44ced290607752fa4848dc7bcd6
Mahmoud Galal <ssss.aaa2002@gmail.com>
2026-04-09 09:06:05 +0200

    feat: deploy

--- apps/platform/lib/api/instance.ts
+++ apps/platform/lib/api/instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://apidalla.kroking.dev',
+  baseURL: 'https://apidalla.snkres.com',
   withCredentials: true,
 })
 
