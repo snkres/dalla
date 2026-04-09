6d03bc57b8ef8d959aabbd18905aa341a5e46011
Mahmoud Galal <ssss.aaa2002@gmail.com>
2026-04-09 09:05:34 +0200

    feat: deploy

--- apps/platform/lib/api/instance.ts
+++ apps/platform/lib/api/instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://devapi.dalla.app',
+  baseURL: 'https://apidalla.kroking.dev',
   withCredentials: true,
 })
 
