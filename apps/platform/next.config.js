9c30b3357435d8305ac9851f74260eaf8f3edc23
AmrTamer23 <amrtamer2324@gmail.com>
2026-04-11 15:12:17 +0200

    chore: minor changes

--- apps/platform/next.config.js
+++ apps/platform/next.config.js
@@ -22,6 +22,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'randomuser.me',
       },
+      {
+        protocol: 'https',
+        hostname: '**.storageapi.dev',
+      },
     ],
   },
 }
